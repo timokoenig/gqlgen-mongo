d1698dd0761f2eff90e824c95b51df5859c58484
Timo Koenig <timo@timokoenig.com>
2021-11-27 19:25:29 +0200

    Adjust mutate hook to accept custom tags defined in the description

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/99designs/gqlgen/api"
 	"github.com/99designs/gqlgen/codegen/config"
@@ -35,11 +37,22 @@ func main() {
 func mutateHook(b *modelgen.ModelBuild) *modelgen.ModelBuild {
 	for _, model := range b.Models {
 		for _, field := range model.Fields {
+			// bson tag
 			name := field.Name
 			if name == "id" {
 				name = "_id"
 			}
 			field.Tag += ` bson:"` + name + `,omitempty"`
+
+			// custom tag
+			description := field.Description
+			scanner := bufio.NewScanner(strings.NewReader(description))
+			for scanner.Scan() {
+				line := strings.TrimSpace(scanner.Text())
+				if strings.HasPrefix(line, "+") {
+					field.Tag += " " + strings.Replace(line, "+", "", 1)
+				}
+			}
 		}
 	}
 
